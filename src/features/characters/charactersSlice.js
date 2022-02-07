e289fc4231facaa744de501837ae0f0097d80ea3
LordHippopotamus <LordHippopotamus@yandex.ru>
2022-02-07 23:02:42 +0000

    feat(charactersSlice): fetch all characters

--- src/features/characters/charactersSlice.js
+++ src/features/characters/charactersSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
   status: 'idle',
@@ -6,10 +7,33 @@ const initialState = {
   list: [],
 };
 
+export const fetchAllCharacters = createAsyncThunk(
+  'characters/fetchAllCharacters',
+  async () => {
+    const request = await fetch('https://futuramaapi.herokuapp.com/api/v2/characters');
+    const json = await request.json();
+    return json;
+  },
+);
+
 const charactersSlice = createSlice({
   name: 'quotes',
   initialState,
   reducers: {},
+  extraReducers: (buider) => {
+    buider
+      .addCase(fetchAllCharacters.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.list = action.payload;
+      })
+      .addCase(fetchAllCharacters.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAllCharacters.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default charactersSlice.reducer;
