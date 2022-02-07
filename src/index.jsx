bdd968707d016a957e0e609271af464b73bea155
LordHippopotamus <LordHippopotamus@yandex.ru>
2022-02-07 22:28:28 +0000

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
+import App from './app/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
