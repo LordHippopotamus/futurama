9e97ebcd11b11b55502b1d4346ef29de787ddded
LordHippopotamus <LordHippopotamus@yandex.ru>
2022-02-07 22:49:29 +0000

    feat(quotesSlice): fetch all quotes

--- src/features/quotes/quotesSlice.js
+++ src/features/quotes/quotesSlice.js
@@ -1,4 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const initialState = {
   status: 'idle',
@@ -6,10 +7,33 @@ const initialState = {
   list: [],
 };
 
+export const fetchAllQuotes = createAsyncThunk(
+  'quotes/fetchQuotes',
+  async () => {
+    const request = await fetch('https://futuramaapi.herokuapp.com/api/quotes/163');
+    const json = await request.json();
+    return json;
+  },
+);
+
 const quotesSlice = createSlice({
   name: 'quotes',
   initialState,
   reducers: {},
+  extraReducers: (buider) => {
+    buider
+      .addCase(fetchAllQuotes.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.list = action.payload;
+      })
+      .addCase(fetchAllQuotes.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAllQuotes.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
+  },
 });
 
 export default quotesSlice.reducer;
