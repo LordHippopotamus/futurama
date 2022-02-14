a7507159b1060c8cd6b6022299abd64037567277
LordHippopotamus <LordHippopotamus@yandex.ru>
2022-02-14 16:57:45 +0000

    feat(Quotes): create pagination

--- src/features/quotes/Quotes.jsx
+++ src/features/quotes/Quotes.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Link, useSearchParams } from 'react-router-dom';
 import Masonry from '@mui/lab/Masonry';
 import CircularProgress from '@mui/material/CircularProgress';
 import Card from '@mui/material/Card';
@@ -7,15 +8,19 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea, Container } from '@mui/material';
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
 import { fetchAllQuotes } from './quotesSlice';
 
 const Quotes = () => {
   const dispatch = useDispatch();
+  const [searchParams] = useSearchParams();
 
   useEffect(() => {
     dispatch(fetchAllQuotes());
   }, [dispatch]);
 
+  const page = +searchParams.get('page') || 1;
   const quotes = useSelector((state) => state.quotes.list);
   const status = useSelector((state) => state.quotes.status);
   const error = useSelector((state) => state.quotes.error);
@@ -32,34 +37,49 @@ const Quotes = () => {
     );
   } else if (status === 'succeeded') {
     content = (
-      <Masonry
-        columns={{
-          xs: 1, sm: 2, md: 3, lg: 4,
-        }}
-        spacing={2}
-      >
-        {quotes.map((item) => (
-          <Card key={item.quote}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                image={item.image}
-                alt="character"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5">{item.character}</Typography>
-                <Typography color="text.secondary">{item.quote}</Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
-      </Masonry>
+      <>
+        <Masonry
+          columns={{
+            xs: 1, sm: 2, md: 3, lg: 4,
+          }}
+          spacing={2}
+        >
+          {quotes.slice((page - 1) * 20, page * 20).map((item) => (
+            <Card key={item.quote}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  image={item.image}
+                  alt="character"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5">{item.character}</Typography>
+                  <Typography color="text.secondary">{item.quote}</Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
+        </Masonry>
+        <Pagination
+          page={page}
+          count={Math.ceil(quotes.length / 20)}
+          color="primary"
+          renderItem={(item) => (
+            <PaginationItem
+              component={Link}
+              to={`/quotes${item.page === 1 ? '' : `?page=${item.page}`}`}
+              // eslint-disable-next-line react/jsx-props-no-spreading
+              {...item}
+            />
+          )}
+        />
+      </>
     );
   }
 
   return (
     <Container sx={{
-      mt: 4, display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '80vh',
+      my: 4, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', minHeight: '80vh',
     }}
     >
       {content}
