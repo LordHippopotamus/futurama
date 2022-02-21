9a130e36657d645dc8197ccd1ff68307dcf1a1d6
LordHippopotamus <LordHippopotamus@yandex.ru>
2022-02-21 11:46:35 +0000

    refactor(Character): dont use navigate function

--- src/features/characters/Characters.jsx
+++ src/features/characters/Characters.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Masonry from '@mui/lab/Masonry';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
@@ -8,33 +8,29 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
 
-const Characters = ({ characters }) => {
-  const navigate = useNavigate();
-
-  return (
-    <Masonry
-      columns={{
-        xs: 1, sm: 2, md: 3, lg: 4,
-      }}
-      spacing={2}
-      sx={{ my: 4 }}
-    >
-      {characters.map((item) => (
-        <Card key={item.Name}>
-          <CardActionArea onClick={() => navigate(item.Name)}>
-            <CardContent>
-              <Typography gutterBottom variant="h5">{item.Name}</Typography>
-              <CardMedia
-                component="img"
-                image={item.PicUrl}
-                alt="chaacter"
-              />
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      ))}
-    </Masonry>
-  );
-};
+const Characters = ({ characters }) => (
+  <Masonry
+    columns={{
+      xs: 1, sm: 2, md: 3, lg: 4,
+    }}
+    spacing={2}
+    sx={{ my: 4 }}
+  >
+    {characters.map((item) => (
+      <Card key={item.Name}>
+        <CardActionArea component={Link} to={item.Name}>
+          <CardContent>
+            <Typography gutterBottom variant="h5">{item.Name}</Typography>
+            <CardMedia
+              component="img"
+              image={item.PicUrl}
+              alt="chaacter"
+            />
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    ))}
+  </Masonry>
+);
 
 export default Characters;
