879996ca75e3910503767e5d52f4ef9439afd790
LordHippopotamus <LordHippopotamus@yandex.ru>
2022-02-21 14:22:49 +0000

    improve linter rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,9 @@ module.exports = {
     'react',
   ],
   rules: {
-    'react/function-component-definition': [2, { namedComponents: 'arrow-function' }],
+    'react/function-component-definition': [2, {
+      namedComponents: 'arrow-function',
+      unnamedComponents: 'arrow-function',
+    }],
   },
 };
